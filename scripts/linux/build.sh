ef974e8654370dfa826d73d939ff043293904cd3
johnoneil <oneil.john@gmail.com>
2020-04-05 13:14:24 -0700

    Parallel builds for linux debug/release/etc

---
+++ scripts/linux/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+# allow custom cmake, make if desired
+CMAKE="${CMAKE:-cmake}"
+MAKE="${MAKE:-make}"
+
+# Build defaults to debug
+# Define an env variable BUILD=Release or other to override
+BUILD="${BUILD:-Debug}"
+
+# Fail if we don't have CMakeLists.txt in current dir
+CMAKELISTS=${PWD}/CMakeLists.txt
+if [ ! -f "$CMAKELISTS" ]; then
+    echo "$CMAKELISTS could not be found."
+    exit -1
+fi
+
+BUILD_DIR=${PWD}/build/${BUILD}
+mkdir -p ${BUILD_DIR}
+${CMAKE} -S . -B ${BUILD_DIR} -DCMAKE_BUILD_TYPE=${BUILD}
+pushd ${BUILD_DIR}
+${MAKE}
